41ab43c8e2640e63d29eeecd754cad33e1a7e676
Olga Chavez <olgakatrin827@gmail.com>
2016-09-18 02:21:41 +0000

    shop-list

---
+++ test/test-server.js
@@ -0,0 +1,8 @@
+describe('Shopping List', function() {
+    it('should list items on get');
+    it('should add an item on post');
+    it('should edit an item on put');
+    it('should delete an item on delete');
+    it('should post to an id that exists');
+    it('should post without body data');
+    });
